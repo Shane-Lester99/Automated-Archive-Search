1a23360d516ed8a8ccdc18102ae15ab25ff3c372
shane <shanejlester@gmail.com>
2019-04-26 12:30:55 -0700

    Add in the interface

--- analysis/create_analysis.py
+++ analysis/create_analysis.py
@@ -23,14 +23,29 @@ def initApp(master, name):
     conf = SparkConf().setMaster(master).setAppName(name)
     return SparkContext(conf=conf)
 
-if __name__ == '__main__':
-    sc = initApp('local','Semantic Similarity')
-    files = ['../test/medium_file.txt', '../test/small_file.txt', '../test/demo.txt']
-    baseDataStructure = makeWordToDocDataStructure(sc, files[1])
-    tfMatrix =  makeTfMatrix(sc, files[1], baseDataStructure) 
-    #AprintRdd(tfMatrix)
-    idfMatrix = makeIdfMatrix(sc, files[1], baseDataStructure)
-    mult = multTfIdf(sc, tfMatrix, idfMatrix)
-    printRdd(mult)
+class SemanticSimilarity:
+    _textFilePath = None 
+    
+#    cli.SwitchAttr('--file-path',
+#                                    mandatory=True,
+#                                    argtype=cli.ExistingFile,
+#                                    help='File to do analysis on')
+    def __init__(self, filePath):
+        self._textFilePath = filePath
+        sc = initApp('local','Semantic Similarity')
+#        files = ['../test/medium_file.txt', '../test/small_file.txt', '../test/demo.txt']
+        if local.path(self._textFilePath).suffix != '.txt':
+            raise ValueError('{0} is not a text file. Exiting'.format(self._textFilePath))
+        baseDataStructure = makeWordToDocDataStructure(sc, self._textFilePath)
+        tfMatrix =  makeTfMatrix(sc, self._textFilePath, baseDataStructure) 
+        #AprintRdd(tfMatrix)
+        idfMatrix = makeIdfMatrix(sc, self._textFilePath, baseDataStructure)
+        mult = multTfIdf(sc, tfMatrix, idfMatrix)
+        printRdd(mult)
 #    printRdd(baseDataStructure)
 
+
+if __name__=='__main__':
+    if len(sys.argv) != 2:
+        raise ValueError('Must have 1 file path argument. Exiting.')
+    SemanticSimilarity(sys.argv[1])
