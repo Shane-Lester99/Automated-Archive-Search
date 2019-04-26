c48169727aa4885e01d8591b3a846168e687e214
shane <shanejlester@gmail.com>
2019-04-26 13:43:40 -0700

    Add interface finished

--- analysis/create_analysis.py
+++ analysis/create_analysis.py
@@ -2,6 +2,8 @@
 
 from plumbum import local, cli
 import sys
+import glob
+import time
 sys.path.append(local.path(__file__).dirname.up())
 from tf_matrix import makeTfMatrix
 from idf_matrix import makeIdfMatrix
@@ -23,29 +25,59 @@ def initApp(master, name):
     conf = SparkConf().setMaster(master).setAppName(name)
     return SparkContext(conf=conf)
 
-class SemanticSimilarity:
-    _textFilePath = None 
-    
-#    cli.SwitchAttr('--file-path',
-#                                    mandatory=True,
-#                                    argtype=cli.ExistingFile,
-#                                    help='File to do analysis on')
-    def __init__(self, filePath):
-        self._textFilePath = filePath
+class SemanticSimilarity: 
+
+    def printMenu(self):
+        print('Command List:\n'
+              'text: will show all words to perform analysis on\n'
+              'retrievetop,<n>,<word>: will output the top n (only up to 5) most similar words desc\n'
+              'help: display this help menu\n')
+
+    def showAllText(self, rdd):
+        items = rdd.collect()
+        for i in items:
+            print(i[0])
+
+    def retrieveTopN(self, num):
+        pass
+
+    def __init__(self, filePath, command, num = None):
         sc = initApp('local','Semantic Similarity')
 #        files = ['../test/medium_file.txt', '../test/small_file.txt', '../test/demo.txt']
-        if local.path(self._textFilePath).suffix != '.txt':
-            raise ValueError('{0} is not a text file. Exiting'.format(self._textFilePath))
-        baseDataStructure = makeWordToDocDataStructure(sc, self._textFilePath)
-        tfMatrix =  makeTfMatrix(sc, self._textFilePath, baseDataStructure) 
+        if local.path(filePath).suffix != '.txt':
+            raise ValueError('{0} is not a text file. Exiting'.format(filePath))
+        print('Initializing calculation. Please wait')
+        time.sleep(2)
+        baseDataStructure = makeWordToDocDataStructure(sc, filePath)
+        tfMatrix =  makeTfMatrix(sc, filePath, baseDataStructure) 
         #AprintRdd(tfMatrix)
-        idfMatrix = makeIdfMatrix(sc, self._textFilePath, baseDataStructure)
+        idfMatrix = makeIdfMatrix(sc, filePath, baseDataStructure)
         mult = multTfIdf(sc, tfMatrix, idfMatrix)
-        printRdd(mult)
+        #printRdd(mult)
+        whitespace(4)
+        
+        if command == 'help':
+            whitespace(4)
+            self.printMenu()
+            whitespace(4)
+        elif command == 'text':
+            whitespace(4)
+            self.showAllText(mult)
+            whitespace(4)
+        elif re.search('retrievetop*', command):
+            print('TTTTTTTTTTTTTTTTT')
+             
+        else: 
+            whitespace(4)
+            print('Invalid command\n')
+            self.printMenu()
+            whitespace(4)
+
+
 #    printRdd(baseDataStructure)
 
 
 if __name__=='__main__':
-    if len(sys.argv) != 2:
-        raise ValueError('Must have 1 file path argument. Exiting.')
-    SemanticSimilarity(sys.argv[1])
+    if len(sys.argv) != 3:
+        raise ValueError('Please enter a file path and command. Exiting.')
+    SemanticSimilarity(sys.argv[1], sys.argv[2])
